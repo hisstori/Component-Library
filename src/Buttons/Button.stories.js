719646227cffebf44c84299bcfae156544be4b8e
Hisstori <browncryan@outlook.com>
2020-02-21 13:49:29 -0500

    Button test

--- src/Buttons/Button.stories.js
+++ src/Buttons/Button.stories.js
@@ -3,3 +3,30 @@ import { storiesOf } from '@storybook/react';
 import './Button.css';
 import Button from './Button';
 
+storiesOf('Button', module)
+    .add('Primary', () => <Button 
+        label="Primary" 
+        type="primary"
+    />) 
+    .add('Danger', () => <Button
+        label="Danger"
+        type="danger"
+    />)
+    .add('Warning', () => <Button
+        label="Warning"
+        type="warning"
+    />)
+    .add('Success', () => <Button
+        label="Success"
+        type="success"        
+    />)
+    .add('Large Primary', () => <Button
+        label="Large Primary"
+        type="primary"
+        large
+    />)
+    .add('Large Danger', () => <Button
+        label="Large Danger"
+        type="danger"
+        large
+    />)
